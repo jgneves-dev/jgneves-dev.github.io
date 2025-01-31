babfa782fc3582ebab24f9896a732973f7c65973
jgneves-dev <joaogneves.dev@gmail.com>
2025-01-31 03:06:31 +0000

    Fix image paths for GitHub Pages

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,56 +8,56 @@ export const Projects = () => {
 
   const projects = [
     {
-      image: "../../assets/bn-thumb.png",
+      image: '${import.meta.env.BASE_URL}assets/bn-thumb.png',
       languages: ["C#", "Unity 6"],
       title: "Bubblenaut",
       description: t('bubblenaut'),
       viewMoreUrl: "https://bigoddstudios.itch.io/bubblenaut"
     },
     {
-      image: "../../assets/hero-image.png",
+      image: '${import.meta.env.BASE_URL}assets/hero-image.png',
       languages: ["JavaScript", "React", "CSS", "HTML"],
       title: "Portfolio",
       description: t('portfolio'),
       viewMoreUrl: "https://jgneves-dev.github.io/"
     },
     {
-      image: "../../assets/cb-thumb.png",
+      image: '${import.meta.env.BASE_URL}assets/cb-thumb.png',
       languages: ["C++", "Unreal Engine 4"],
       title: "Crazy Bus!",
       description: t('crazy_bus'),
       viewMoreUrl: "https://crazybus-finalproject.itch.io/crazy-bus"
     },
     {
-      image: "../../assets/rom-thumb.png",
+      image: '${import.meta.env.BASE_URL}assets/rom-thumb.png',
       languages: ["C++, Unreal Engine 4"],
       title: "Rooms of Mystery",
       description: t('rooms_of_mystery'),
       viewMoreUrl: "https://coffecatgames.itch.io/rooms-of-mystery"
     },
     {
-      image: "../../assets/fs-thumb.png",
+      image: '${import.meta.env.BASE_URL}assets/fs-thumb.png',
       languages: ["C#", "Unity"],
       title: "Forgotten Shrine",
       description: t('forgotten_shrine'),
       viewMoreUrl: "https://edgypress-studio.itch.io/forgotten-shrine"
     },
     {
-      image: "../../assets/bat-thumb.png",
+      image: '${import.meta.env.BASE_URL}assets/bat-thumb.png',
       languages: ["GBStudio"],
       title: "By a Thread",
       description: t('by_a_thread'),
       viewMoreUrl: "https://tomous.itch.io/by-a-thread"
     },
     {
-      image: "../../assets/ff-thumb.png",
+      image: '${import.meta.env.BASE_URL}assets/ff-thumb.png',
       languages: ["C#", "Unity"],
       title: "Fool's Frenzy",
       description: t('fools_frenzy'),
       viewMoreUrl: "https://edgypress-studio.itch.io/fools-frenzy"
     },
     {
-      image: "../../assets/sf-thumb.png",
+      image: '${import.meta.env.BASE_URL}assets/sf-thumb.png',
       languages: ["HTML", "CSS", "JavaScript", "PHP", "MySQL", "Bootstrap", "Weebly"],
       title: "Stray Friends",
       description: t('stray_friends'),
