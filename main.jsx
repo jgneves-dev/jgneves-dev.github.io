05b459305cbb3c7ab726e629f9d594980b2355e2
jgneves-dev <joaogneves.dev@gmail.com>
2025-01-31 02:36:10 +0000

    Main update

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './src/App';
-import './styles/global.css';
+import './src/styles/global.css';
 import './src/i18n';
 
 const container = document.getElementById('root');
