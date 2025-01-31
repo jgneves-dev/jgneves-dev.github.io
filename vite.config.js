e7909c0c4c92e4ef15cee7da5777bae10a7fea52
jgneves-dev <joaogneves.dev@gmail.com>
2025-01-31 01:41:59 +0000

    Config update

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/jgneves-dev.github.io/',
+  base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
   },
   plugins: [react()],
   css: {
